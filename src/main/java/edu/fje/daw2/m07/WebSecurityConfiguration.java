e6deddc146159dafa22af8581938b35f3fd1763c
sergigrau <sergi.grau@fje.edu>
2021-04-16 12:37:00 +0200

    oauth2 incorporated

---
+++ src/main/java/edu/fje/daw2/m07/WebSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package edu.fje.daw2.m07;
+
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/index.html", "/spring-logo.svg")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
